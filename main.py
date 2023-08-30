4d5daa976e7c9ab4c95d6a2de180642914544a4c
Jocim <joe.susatiyo@gmail.com>
2023-08-30 15:52:11 +0700

    removed rounded rectangle function

--- main.py
+++ main.py
@@ -88,31 +88,7 @@ class Calc():
 		self.result = False
 		self.current = -(float(txtDisplay.get()))
 		self.display(self.current)
-	def round_rectangle(x1, y1, x2, y2, radius=25, **kwargs):
-        
-		points = [x1+radius, y1,
-				x1+radius, y1,
-				x2-radius, y1,
-				x2-radius, y1,
-				x2, y1,
-				x2, y1+radius,
-				x2, y1+radius,
-				x2, y2-radius,
-				x2, y2-radius,
-				x2, y2,
-				x2-radius, y2,
-				x2-radius, y2,
-				x1+radius, y2,
-				x1+radius, y2,
-				x1, y2,
-				x1, y2-radius,
-				x1, y2-radius,
-				x1, y1+radius,
-				x1, y1+radius,
-				x1, y1]
-
-		return calc.create_polygon(points, **kwargs, smooth=True)
-
+		
 added_value = Calc()
 
 txtDisplay = Entry(calc, font=('Helvetica',20,'bold'),
